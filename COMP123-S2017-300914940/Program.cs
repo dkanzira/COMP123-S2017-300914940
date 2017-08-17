e53050c0c023f3d8e82bd13aa61ba44f6b121b74
Dennis Kanzira <Dennis Kanzira@LAPTOP-E0G5NNPS>
2017-08-17 13:06:58 -0400

    Created Splash Form, Run the SplashForm

--- COMP123-S2017-300914940/Program.cs
+++ COMP123-S2017-300914940/Program.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 /*
- * Name:
- * Date:
- * StudentID:
- * Description:
- * Version:
+ * Name:Dennis Kanzira
+ * Date:08/17/2017
+ * StudentID:300914940
+ * Description:This is the Driver Class for the Card Game
+ * Version: 0.1 - Running the Splash Form
  */
 
 namespace COMP123_S2017_300914940
@@ -24,7 +24,7 @@ namespace COMP123_S2017_300914940
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PickHighestCardForm());
+            Application.Run(new SplashForm());
         }
     }
 }
---
+++ COMP123-S2017-300914940/SplashForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name:Dennis Kanzira
+ * Student ID#:300914940    
+ * Subject:Comp123
+ * Event:Final Exam Summer 2017
+ * Date:08/17/2017
+ * Description: This is the splash Form for the Card Game
+ * Version: 0.1 - created the Splash Form
+ * 
+ */ 
+namespace COMP123_S2017_300914940
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
